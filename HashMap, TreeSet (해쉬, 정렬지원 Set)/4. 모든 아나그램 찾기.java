767aabf9b7f955d3fc1e1df1304d9922274ee78f
kingkid159 <kingkid123@naver.com>
2024-12-27 18:58:04 +0900

    4. 모든 아나그램 찾기

--- HashMap, TreeSet (해쉬, 정렬지원 Set)/4. 모든 아나그램 찾기.java
+++ HashMap, TreeSet (해쉬, 정렬지원 Set)/4. 모든 아나그램 찾기.java
@@ -9,6 +9,60 @@
  * 
  * S단어에 T문자열과 아나그램이 되는 부분문자열의 개수를 출력합니다.
  */
+/**
+ * 4. 모든 아나그램 찾기
+ * 
+ * S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하는 프로그램을 작성하세요.
+ * 아나그램 판별시 대소문자가 구분됩니다. 부분문자열은 연속된 문자열이어야 합니다.
+ * 
+ * 첫 줄에 첫 번째 S문자열이 입력되고, 두 번째 줄에 T문자열이 입력됩니다.
+ * S문자열의 길이는 10,000을 넘지 않으며, T문자열은 S문자열보다 길이가 작거나 같습니다.
+ * 
+ * S단어에 T문자열과 아나그램이 되는 부분문자열의 개수를 출력합니다.
+ */
+
+import java.util.*;
+  
+public class Main {
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    String input1 = in.next();
+    String input2 = in.next();
+
+    Map<Character, Integer> sMap = new HashMap<>();
+    Map<Character, Integer> tMap = new HashMap<>();
+    int lt = 0;
+    int rt = 0;
+    int result = 0;
+
+    for(char x : input2.toCharArray()){
+      tMap.put(x, tMap.getOrDefault(x, 0) + 1);
+    }
+
+    for(char x : input1.toCharArray()){
+      sMap.put(x, sMap.getOrDefault(x, 0) + 1);
+      if(rt - lt  + 1 > input2.length()){
+        char leftChar = input1.charAt(lt);
+        sMap.put(leftChar, sMap.get(leftChar) - 1);
+        if(sMap.get(leftChar) == 0){
+          sMap.remove(leftChar);
+        }
+        lt++;
+      }
+      if(tMap.equals(sMap)){ // map끼리 equals로 비교하게 되면 key와 value가 모두 같은 map일 경우 true 리턴
+        result++;
+      }
+      rt++;
+    }
+
+    System.out.println(result);
+    return ;
+  }
+}
+
+
+
+//////////////////////////////////////////////////////////////////////
 import java.util.*;
   
 public class Main {
